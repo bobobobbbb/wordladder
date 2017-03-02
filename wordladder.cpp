3eaf9afe79b49ed7ac6aa3a8ba68496322ed4d15
bofan li <1105388289@qq.com>
2017-03-02 21:21:06 +0800

    intitial

---
+++ wordladder.cpp
@@ -0,0 +1,25 @@
+// This is the CPP file you will edit and turn in.
+// Also remove these comments here and add your own.
+// TODO: remove this comment header
+
+#include <cctype>
+#include <cmath>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include "console.h"
+
+using namespace std;
+
+//be careful
+int main() {
+    // TODO: Finish the program!
+    cout << "Welcome to CS 106B Word Ladder." << endl;
+    cout << "Please give me two English words, and I will change the" << endl;
+    cout << "first into the second by changing one letter at a time." << endl;
+
+
+    cout << "Have a nice day." << endl;
+    return 0;
+}
+
